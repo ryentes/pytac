dd1334b93c7ef21b50efccc703cefa1104938a00
Richard Yentes <ryentes@gmail.com>
2025-06-07 00:29:53 -0400

    removing fields that will never be populated by api from abilities model.

--- src/pytac/models/Ability.py
+++ src/pytac/models/Ability.py
@@ -4,9 +4,8 @@ import json # for dev purposes only; replace with api call
 
 class Ability(BaseModel):
     id: str
-    level: Optional[Union[None, int]]=None
-    tooltip: Optional[Union[None, str]]=None
-    type: Optional[Union[None, str]]=None
+    level: Optional[Union[None, int]]
+
 
     def supplement_info(self, other: "Ability") -> "Ability":
         m = self.model_dump(
